f88df3dd71b53682323f206e5c0442ac7f639d5d
riskiip <muhamadrizki19@student.undip.ac.id>
2020-08-04 17:30:55 +0700

    Register works perfectly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +38,8 @@ import { PageNotFoundComponent } from './components/shared/page-not-found/page-n
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
+
+// tslint:disable-next-line: typedef
+function passwordMatchValidator(form) {
+  const password = form.get('password');
+  const confirmPassword = form.get('confirmPassword');
+
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.setErrors( {passwordMatch: true});
+  } else {
+    confirmPassword.setErrors(null);
+  }
+
+  return null;
+}
+
+// tslint:disable-next-line: typedef
+function symbolValidator(control) {
+  if (control.hasError('required')) {
+    return null;
+  }
+  if (control.hasError('minlength')) {
+    return null;
+  }
+
+  if (control.value.indexOf('@') > -1) {
+    return null;
+  } else {
+    return { symbol: true };
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -7,9 +38,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private builder: FormBuilder) { }
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.buildingForm();
+  }
+
+  // tslint:disable-next-line: typedef
+  buildingForm() {
+    this.registerForm = this.builder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, symbolValidator, Validators.minLength(4)]],
+      confirmPassword: ''
+    }, {
+      validators: passwordMatchValidator
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  register() {
+    console.log(this.registerForm.value);
   }
 
 }
