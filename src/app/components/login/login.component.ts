2f9d936d99f605d065e55993f0e9626ab5c9364e
riskiip <muhamadrizki19@student.undip.ac.id>
2020-08-04 16:04:37 +0700

    We'll continue from this

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,17 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   model: any = {};
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   login() {
     console.log(this.model);
